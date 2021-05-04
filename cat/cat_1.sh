e0f6fba59f96ee23c7f08255434f3f41e3cfdcc7
DavidFliguer <david.fliguer@gmail.com>
2021-05-04 17:01:33 +0300

    Adding example of cat

---
+++ cat/cat_1.sh
@@ -0,0 +1,7 @@
+# Using cat to show content of files
+
+# Show only the content of file 1
+cat some_file_1.txt
+
+# Show content of file 1 and 2 together ("Concatenated")
+cat some_file_1.txt some_file_2.txt 
